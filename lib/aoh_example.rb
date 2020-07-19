4862a30997f6d095387f0f096ae44aeba9bf394e
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-19 23:27:43 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -32,6 +32,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   aoh[1][:name] = "Joelle van Dyne (PGOAT)"
+  return aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
